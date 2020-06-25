ce1a7008c225dbd45ddb80c127ffc25626939de4
lugrao <elugrao@gmail.com>
2020-06-25 15:08:35 -0300

    agrega meta imagen

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,6 +7,7 @@ export default function Layout({ children }) {
         <title>colornames feed</title>
         <meta property="og:title" content="cien colores random" key="title" />
         <meta name="description" content="cien colores random nombrados por cien personas random" />
+        <meta property="og:image" content="/siete-colores.svg" />
         <link rel="icon" href="/rueda-de-colores.svg" />
       </Head>
       <main id="main">{children}</main>
