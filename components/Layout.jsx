4a58b854066d8a778ddf03c3bd96f8b29d506ba2
lugrao <elugrao@gmail.com>
2020-06-25 12:31:50 -0300

    agrega meta description

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,7 +5,8 @@ export default function Layout({ children }) {
     <div>
       <Head>
         <title>colornames feed</title>
-        <meta property="og:title" content="colornames feed" key="title" />
+        <meta property="og:title" content="cien colores random" key="title" />
+        <meta name="cien colores random nombrados por cien personas random" />
         <link rel="icon" href="/rueda-de-colores.svg" />
       </Head>
       <main id="main">{children}</main>
