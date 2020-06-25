63d724ee5df58e43b8273dc0819f4349a0583407
lugrao <elugrao@gmail.com>
2020-06-25 15:10:26 -0300

    modifica meta imagen

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,8 +6,14 @@ export default function Layout({ children }) {
       <Head>
         <title>colornames feed</title>
         <meta property="og:title" content="cien colores random" key="title" />
-        <meta name="description" content="cien colores random nombrados por cien personas random" />
-        <meta property="og:image" content="/siete-colores.svg" />
+        <meta
+          name="description"
+          content="cien colores random nombrados por cien personas random"
+        />
+        <meta
+          property="og:image"
+          content="https://upload.wikimedia.org/wikipedia/commons/b/b4/Seven-colors_%28visible_spectrum%29_vector.svg"
+        />
         <link rel="icon" href="/rueda-de-colores.svg" />
       </Head>
       <main id="main">{children}</main>
