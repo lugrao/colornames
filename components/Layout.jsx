5c6616225969d44e9df76c75bf2e90f5c5053ada
lugrao <elugrao@gmail.com>
2020-06-25 12:39:34 -0300

    corrige meta desc

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,7 +6,7 @@ export default function Layout({ children }) {
       <Head>
         <title>colornames feed</title>
         <meta property="og:title" content="cien colores random" key="title" />
-        <meta name="cien colores random nombrados por cien personas random" />
+        <meta name="description" content="cien colores random nombrados por cien personas random" />
         <link rel="icon" href="/rueda-de-colores.svg" />
       </Head>
       <main id="main">{children}</main>
